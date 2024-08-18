85ec7c1c71009271c058246893610645d2e27f9d
afzalimdad9 <122292180+afzalimdad9@users.noreply.github.com>
2024-08-18 12:34:14 +0500

    added test cases, all passing

---
+++ test/__init__.py
@@ -0,0 +1,9 @@
+'''
+Unit Test Cases for JSON2HTML
+Description - python wrapper for converting JSON to HTML Table format
+(c) 2024 Afzal Imdad. MIT License
+'''
+
+__author__ = 'Afzal Imdad'
+__version__ = '1.0.0'
+__license__ = 'MIT'
