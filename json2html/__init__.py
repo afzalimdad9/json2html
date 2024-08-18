c7f77323708ceb957d1beb8842f8be0098f83c9c
afzalimdad9 <122292180+afzalimdad9@users.noreply.github.com>
2024-08-18 17:32:00 +0500

    making json2html 1.0.3 python3 compatible and trying to integrate tests to check for this in the future

--- json2html/__init__.py
+++ json2html/__init__.py
@@ -3,7 +3,7 @@ python wrapper for JSON to HTML-Table convertor
 (c) 2024 Afzal Imdad. MIT License
 """
 
-from jsonconv import *
+from .jsonconv import *
 
 __author__ = 'Afzal Imdad'
 __version__ = '1.0.3'
